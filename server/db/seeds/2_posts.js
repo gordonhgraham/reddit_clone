647a2b487774badbbe31c2d9c1cd53478629d463
Gordon H Graham <theboss@gordonhgraham.com>
2016-12-02 11:44:22 -0700

    add path to knexfile.js for heroku postbuild

--- server/db/seeds/2_posts.js
+++ server/db/seeds/2_posts.js
@@ -4,9 +4,9 @@ exports.seed = function(knex, Promise) {
   return knex('posts').del()
     .then(function () {
       return Promise.all([
-        knex('posts').insert({user_id: 1, title: `Post 1`, votes: 1, content: `This is the first post.`, photo_url: `https://source.unsplash.com/random/300x200`}),
-        knex('posts').insert({user_id: 1, title: `Post 2`, votes: 2, content: `This is the second post.`, photo_url: `https://source.unsplash.com/random/300x200`}),
-        knex('posts').insert({user_id: 1, title: `Post 3`, votes: 3, content: `This is the third post.`, photo_url: `https://source.unsplash.com/random/300x200`})
+        knex('posts').insert({user_id: 1, title: `Post 1`, votes: 1, content: `This is the first post.`, photo_url: `https://source.unsplash.com/random/300x201`}),
+        knex('posts').insert({user_id: 1, title: `Post 2`, votes: 2, content: `This is the second post.`, photo_url: `https://source.unsplash.com/random/300x202`}),
+        knex('posts').insert({user_id: 1, title: `Post 3`, votes: 3, content: `This is the third post.`, photo_url: `https://source.unsplash.com/random/300x203`})
       ])
     })
 }
